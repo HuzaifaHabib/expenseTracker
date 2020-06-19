fabb40326c187df025d4c3e32986af5e8036907b
Huzaifa Habib <huzaifa.dj@gmail.com>
2020-06-19 16:29:08 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Child from './child'
+
+function App() {
+  return (
+    <div >
+      <Child />
+    </div>
+  );
+}
+
+export default App;
