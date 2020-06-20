313d009fc6be3eb0ed6c47ae1b6642f43bafb4e9
Huzaifa Habib <huzaifa.dj@gmail.com>
2020-06-20 16:55:42 +0500

    Trnsaction functionalities

--- src/child.js
+++ src/child.js
@@ -1,32 +1,52 @@
 import React, { useState } from 'react';
 import './App.css';
-import {TransactionContext} from './transContext' 
+import { TransactionContext } from './transContext'
 import { useContext } from 'react';
 
 
 function App() {
 
-    let {transactions , addTransaction} = useContext(TransactionContext);
+    let { transactions, addTransaction } = useContext(TransactionContext);
     let [newDesc, setDesc] = useState("");
-    let [newAmount, setAmount ] = useState(0);
+    let [newAmount, setAmount] = useState(0);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(newDesc, newAmount);
         addTransaction({
-            amount: newAmount,
-            desc: newDesc
+            amount: Number(newAmount),
+            desc: String(newDesc)
         })
+    }
+
+    const getIncome = () => {
+        let income = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount > 0) {
+                income += transactions[i].amount
+            }
+        }
+        return income;
+    }
+
+    const getExpense = () => {
+        let expense = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount < 0) {
+                expense += transactions[i].amount
+            }
+        }
+        return expense;
     }
 
     return (
         <div className="container" >
             <h1 className="text-center">Expense Tracker</h1>
-            <h3>Your balance <br /> $250</h3>
+    <h3>Your balance <br /> {getIncome() + getExpense()}</h3>
 
             <div className="expense-contaner">
-                <h3>Your Income <br /> $500</h3>
-                <h3>Your Expense <br /> $350</h3>
+                <h3>Your Income <br /> {getIncome()}</h3>
+                <h3>Your Expense <br /> {getExpense()}</h3>
 
             </div>
             <h3>History</h3>
@@ -41,7 +61,7 @@ function App() {
                         </li>
                     )
                 })}
-               
+
             </ul>
             <h3>Add new Transaction</h3>
             <hr />
@@ -55,7 +75,7 @@ function App() {
                     Enter Amount <br />
                     <input type="text" required={true} onChange={(ev) => setAmount(ev.target.value)} />
                 </label>
-                <input type="submit" value="Add Transaction"  />
+                <input type="submit" value="Add Transaction" />
             </form>
         </div>
     );
